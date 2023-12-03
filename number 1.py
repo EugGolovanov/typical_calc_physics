d02c56c578914fa9276e3795691565a2f4be6600
Eugene Golovanov <86769060+EugGolovanov@users.noreply.github.com>
2023-12-03 16:52:39 +0300

    Delete number 1.py

--- number 1.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-g = np.float64(9.80665)
-a = np.float64(2)
-m = np.float64(0.2)
-alpha = np.arctan(g/a)
-d_alpha = alpha * (0.00001/g)
-F = m*np.sqrt(np.power(a, 2) + np.power(g, 2))
-print('alpha =', round(alpha, 6), '+-', round(d_alpha,6))
-print('F = ', round(F, 6), '+- 1e-06')
